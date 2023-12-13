85fa45fd91c1d934e4bbaef51a695b44551d043e
imufiid <imufiid98@gmail.com>
2023-12-14 06:46:44 +0700

    feat: Setup navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import {CategoriesScreen} from "./src/screen/CategoriesScreen";
 import {StatusBar} from "expo-status-bar";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <>
-      <StatusBar style={'light'}/>
-      <CategoriesScreen/>
+      <StatusBar style={'dark'}/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Categories'
+            component={CategoriesScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
